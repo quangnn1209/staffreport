17a08943137a4e311c7cea129f3a5d89c93c085f
Quang <quangnn1209@gmail.com>
2015-01-28 08:37:58 +0700

    update sql

--- databasescripts/staffreport.sql
+++ databasescripts/staffreport.sql
@@ -1,138 +1,170 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*
+Navicat MySQL Data Transfer
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema staffreport
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `staffreport` DEFAULT CHARACTER SET utf8 ;
-USE `staffreport` ;
+Source Server         : localhost
+Source Server Version : 50619
+Source Host           : localhost:3306
+Source Database       : staffreport
 
--- -----------------------------------------------------
--- Table `staffreport`.`division`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `staffreport`.`division` ;
+Target Server Type    : MYSQL
+Target Server Version : 50619
+File Encoding         : 65001
 
-CREATE TABLE IF NOT EXISTS `staffreport`.`division` (
-  `division_id` INT NOT NULL AUTO_INCREMENT,
-  `division_name` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`division_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+Date: 2015-01-28 08:35:53
+*/
 
+SET FOREIGN_KEY_CHECKS=0;
 
--- -----------------------------------------------------
--- Table `staffreport`.`team`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `staffreport`.`team` ;
-
-CREATE TABLE IF NOT EXISTS `staffreport`.`team` (
-  `team_id` INT NOT NULL AUTO_INCREMENT,
-  `team_name` VARCHAR(30) NOT NULL,
-  `create_date` DATE NOT NULL,
-  `end_date` DATE NULL DEFAULT NULL,
-  `division_id` INT NOT NULL,
-  PRIMARY KEY (`team_id`),
-  INDEX `fk_team_division1_idx` (`division_id` ASC),
-  CONSTRAINT `fk_team_division1`
-    FOREIGN KEY (`division_id`)
-    REFERENCES `staffreport`.`division` (`division_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `staffreport`.`Employees`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `staffreport`.`Employees` ;
-
-CREATE TABLE IF NOT EXISTS `staffreport`.`Employees` (
-  `emp_id` INT NOT NULL AUTO_INCREMENT,
-  `emp_full_name` VARCHAR(30) NOT NULL,
-  `join_date` DATE NOT NULL,
-  `emp_skills` TEXT NULL,
-  `emp_birthday` DATE NULL,
-  `emp_phone` VARCHAR(20) NULL,
-  `emp_contact` VARCHAR(50) NULL,
-  `emp_avatar` VARCHAR(100) NULL,
-  `emp_status` INT NULL,
-  `division_id` INT NULL,
-  `team_id` INT NULL,
-  PRIMARY KEY (`emp_id`),
-  INDEX `fk_Employees_division1_idx` (`division_id` ASC),
-  INDEX `fk_Employees_team1_idx` (`team_id` ASC),
-  CONSTRAINT `fk_Employees_division1`
-    FOREIGN KEY (`division_id`)
-    REFERENCES `staffreport`.`division` (`division_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Employees_team1`
-    FOREIGN KEY (`team_id`)
-    REFERENCES `staffreport`.`team` (`team_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `staffreport`.`EmployeeHistory`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `staffreport`.`EmployeeHistory` ;
-
-CREATE TABLE IF NOT EXISTS `staffreport`.`EmployeeHistory` (
-  `emp_history_id` INT NOT NULL AUTO_INCREMENT,
-  `team_name` VARCHAR(30) NULL,
-  `division_name` VARCHAR(30) NULL,
-  `start_date` DATE NOT NULL,
-  `end_date` DATE NULL,
-  `emp_id` INT NOT NULL,
-  PRIMARY KEY (`emp_history_id`),
-  INDEX `fk_EmployeeHistory_Employees1_idx` (`emp_id` ASC),
-  CONSTRAINT `fk_EmployeeHistory_Employees1`
-    FOREIGN KEY (`emp_id`)
-    REFERENCES `staffreport`.`Employees` (`emp_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `staffreport`.`Admin`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `staffreport`.`Admin` ;
-
-CREATE TABLE IF NOT EXISTS `staffreport`.`Admin` (
-  `admin_id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(30) NOT NULL,
-  `password` VARCHAR(30) NOT NULL,
-  `emp_id` INT NULL,
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `emp_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`admin_id`),
-  INDEX `fk_Admin_Employees1_idx` (`emp_id` ASC),
-  CONSTRAINT `fk_Admin_Employees1`
-    FOREIGN KEY (`emp_id`)
-    REFERENCES `staffreport`.`Employees` (`emp_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_Admin_Employees1_idx` (`emp_id`),
+  CONSTRAINT `fk_Admin_Employees1` FOREIGN KEY (`emp_id`) REFERENCES `employees` (`emp_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES ('1', 'admin', 'admin', '1');
 
--- -----------------------------------------------------
--- Table `staffreport`.`test`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `staffreport`.`test` ;
+-- ----------------------------
+-- Table structure for division
+-- ----------------------------
+DROP TABLE IF EXISTS `division`;
+CREATE TABLE `division` (
+  `division_id` int(11) NOT NULL AUTO_INCREMENT,
+  `division_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`division_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `staffreport`.`test` (
-  `test_id` INT NOT NULL AUTO_INCREMENT,
-  `test_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`test_id`))
-ENGINE = InnoDB;
+-- ----------------------------
+-- Records of division
+-- ----------------------------
+INSERT INTO `division` VALUES ('1', 'division1');
+INSERT INTO `division` VALUES ('2', 'division2');
+INSERT INTO `division` VALUES ('3', 'division3');
 
+-- ----------------------------
+-- Table structure for employeehistory
+-- ----------------------------
+DROP TABLE IF EXISTS `employeehistory`;
+CREATE TABLE `employeehistory` (
+  `emp_history_id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_name` varchar(30) DEFAULT NULL,
+  `division_name` varchar(30) DEFAULT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `emp_id` int(11) NOT NULL,
+  PRIMARY KEY (`emp_history_id`),
+  KEY `fk_EmployeeHistory_Employees1_idx` (`emp_id`),
+  CONSTRAINT `fk_EmployeeHistory_Employees1` FOREIGN KEY (`emp_id`) REFERENCES `employees` (`emp_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- ----------------------------
+-- Records of employeehistory
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for employees
+-- ----------------------------
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+  `emp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `emp_full_name` varchar(30) NOT NULL,
+  `join_date` date NOT NULL,
+  `emp_skills` text,
+  `emp_birthday` date DEFAULT NULL,
+  `emp_phone` varchar(20) DEFAULT NULL,
+  `emp_contact` varchar(50) DEFAULT NULL,
+  `emp_avatar` varchar(100) DEFAULT NULL,
+  `emp_status` int(11) DEFAULT NULL,
+  `division_id` int(11) DEFAULT NULL,
+  `team_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`emp_id`),
+  KEY `fk_Employees_division1_idx` (`division_id`),
+  KEY `fk_Employees_team1_idx` (`team_id`),
+  CONSTRAINT `fk_Employees_division1` FOREIGN KEY (`division_id`) REFERENCES `division` (`division_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Employees_team1` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of employees
+-- ----------------------------
+INSERT INTO `employees` VALUES ('1', 'admin', '2015-01-26', null, null, null, null, null, null, '1', '1');
+INSERT INTO `employees` VALUES ('2', 'user', '2015-01-20', 'skills', null, 'phone', 'contact', 'avatar', '0', '2', '2');
+INSERT INTO `employees` VALUES ('3', 'quangnn', '2015-01-27', 'skills', null, 'phone', 'contact', 'avatar', '0', '3', '3');
+
+-- ----------------------------
+-- Table structure for file
+-- ----------------------------
+DROP TABLE IF EXISTS `file`;
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(50) NOT NULL,
+  `content` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of file
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for image
+-- ----------------------------
+DROP TABLE IF EXISTS `image`;
+CREATE TABLE `image` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `file_id` (`file_id`),
+  CONSTRAINT `image_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of image
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for team
+-- ----------------------------
+DROP TABLE IF EXISTS `team`;
+CREATE TABLE `team` (
+  `team_id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_name` varchar(30) NOT NULL,
+  `create_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `division_id` int(11) NOT NULL,
+  PRIMARY KEY (`team_id`),
+  KEY `fk_team_division1_idx` (`division_id`),
+  CONSTRAINT `fk_team_division1` FOREIGN KEY (`division_id`) REFERENCES `division` (`division_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of team
+-- ----------------------------
+INSERT INTO `team` VALUES ('1', 'gulliver', '2015-01-20', null, '1');
+INSERT INTO `team` VALUES ('2', 'god', '2015-01-19', null, '2');
+INSERT INTO `team` VALUES ('3', 'psa', '2015-01-20', null, '3');
+
+-- ----------------------------
+-- Table structure for test
+-- ----------------------------
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+  `test_id` int(11) NOT NULL AUTO_INCREMENT,
+  `test_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`test_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of test
+-- ----------------------------
